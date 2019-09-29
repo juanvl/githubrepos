b6314138ea0dd95752572c15504ad2df13726f91
Juan <jnvleite@gmail.com>
2019-09-29 11:24:05 -0300

    Updates

---
+++ precache-manifest.4dfb6792a3ef0f0d652f5b03155d33b4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "884e884750f7f2a66ca86c3482102aaf",
+    "url": "/juanvl/githubrepos/index.html"
+  },
+  {
+    "revision": "132237ec756f26054de0",
+    "url": "/juanvl/githubrepos/static/js/2.3c873622.chunk.js"
+  },
+  {
+    "revision": "f70c115b6b4ccbd5515b",
+    "url": "/juanvl/githubrepos/static/js/main.81637b51.chunk.js"
+  },
+  {
+    "revision": "98a7007b24b729dbc587",
+    "url": "/juanvl/githubrepos/static/js/runtime~main.83df4663.js"
+  }
+]);
